c94b4b849ad2fa982ed2dcd5a8abebb5f9024389
Anthony Ioimo <anthonylioimo@gmail.com>
2019-02-19 23:39:48 +0100

    configure async request to server and render error messages

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import LoginForm from "../forms/LoginForm";
+import { login } from "../../actions/auth";
 
 class LoginPage extends Component {
-  submit = data => {
-    console.log(data);
-  };
+  submit = data =>
+    this.props.login(data).then(() => this.props.history.push("/"));
 
   render() {
+    console.log("render LoginPage:", this.props.login);
     return (
       <div className="LoginPage">
         <h1>Login Page</h1>
@@ -17,4 +20,14 @@ class LoginPage extends Component {
   }
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+  login: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired
+};
+
+export default connect(
+  null,
+  { login }
+)(LoginPage);
